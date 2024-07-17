88fabf8836880f47ecb9818cdd1b269a20c05502
Leart Deda <leartdeda@icloud.com>
2024-07-17 12:09:11 +0200

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { VideofilesComponent } from './videofiles/videofiles.component';
+import { PlayerComponent } from './player/player.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'video', component: VideofilesComponent },
+  { path: 'player', component: PlayerComponent },
+  // Add more routes as needed
+]
---
+++ src/app/input-handler/input-handler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-handler',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-handler.component.html',
+  styleUrl: './input-handler.component.css'
+})
+export class InputHandlerComponent {
+
+}
