422f51f4e98c44f3d999bc47d44c63e1542467cc
Leart Deda <leartdeda@icloud.com>
2024-07-17 14:52:17 +0200

    finished looping video added if to go button

--- src/app/input-handler/input-handler.component.ts
+++ src/app/input-handler/input-handler.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input-handler',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './input-handler.component.html',
   styleUrls: ['./input-handler.component.css']
 })
 export class InputHandlerComponent {
   userInput: string = '';
-  showVideo: boolean = true;
+  showVideo: boolean = false;
 
   constructor(private router: Router) {}
 
   navigate() {
+    console.log(this.showVideo);
+    
     if (this.showVideo) {
       this.router.navigate(['/video']);
     } else {
